1b5e385adc5f8ac4800765decbdd12093dd6816c
Me <pdprince.lfc+github@gmail.com>
2024-06-11 18:35:19 -0400

    Remove unnecessary code from utility scripts

--- global/js/consts.js
+++ global/js/consts.js
@@ -1,5 +1,4 @@
 export const ENDPOINT = 'https://my-site-server-ec5c73b08ce0.herokuapp.com';
-export const AUTH_ENDPOINT = `${ENDPOINT}/auth`;
 export const BUBBLEGAME_ENDPOINT = `${ENDPOINT}/bubblegame`;
 export const DB_VERSION = 5;
 
